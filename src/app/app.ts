d22a66bf6f5c5cbbc7bea457506b8317e54734ab
AnthonyC93 <anthonym.cartagena@gmail.com>
2025-12-12 00:36:39 -0500

    added fix for safe area

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,28 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('davna-sweet');
+  details = [
+    {
+      title: 'Date & time',
+      img: '',
+      list: ['Sunday', 'March 8, 2026', '4:30PM - 9:30PM']
+    },
+    {
+      title: 'Location',
+      img: '',
+      list: ['Liberty House', '76 Audrey Zapp Dr', 'Jersey City, NJ']
+    },
+    {
+      title: 'Dress Code',
+      img: '',
+      list: ['Winter Theme', "Please do not wear bluie"]
+    }
+  ]
+
 }
