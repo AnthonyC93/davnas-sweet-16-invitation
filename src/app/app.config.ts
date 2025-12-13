ca4164553f00dccd88fd4cdb9962b1a642b4e8f0
AnthonyC93 <anthonym.cartagena@gmail.com>
2025-12-12 22:56:36 -0500

    website in usuable state

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,10 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter, withHashLocation } from '@angular/router';
-
+import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { HashLocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes, withHashLocation()),
+    provideRouter(routes),
   ]
 };
